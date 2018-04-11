dac1b78b0b25fac5c70e5df2639166147c842c10
vvirpik <ach@sybis.ru>
2018-04-11 19:49:18 +0700

    [DEV] Add support method to UINavigationController

--- src/[UINavigationController]/[UINavigationController][T][Appearance].swift
+++ src/[UINavigationController]/[UINavigationController][T][Appearance].swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+public extension UINavigationController {
+    
+    /// Удаляет линию под navigation bar
+    /// [ВАЖНО] isTranslucent = false
+    public func clearBottomLine() {
+        
+        self.navigationBar.isTranslucent = false
+        self.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationBar.shadowImage = UIImage()
+    }
+    
+    /// Сбрасывает линию под navigation bar
+    public func resetBottomLine() {
+        self.navigationBar.setBackgroundImage(nil, for: .default)
+        self.navigationBar.shadowImage = nil
+    }
+}
