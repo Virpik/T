5725544276ea36891618c266711eee06bcc0fb96
vvirpik <ach@sybis.ru>
2018-06-06 11:43:14 +0700

    [DEV] CGSize inti

---
+++ src/[CGSize]/[CGSize][T].swift
@@ -0,0 +1,14 @@
+//
+//  [CGSize][T].swift
+//  T
+//
+//  Created by Virpik on 06/06/2018.
+//
+
+import Foundation
+
+public extension CGSize {
+    public init(_ w: CGFloat, _ h: CGFloat) {
+        self.init(width: w, height: h)
+    }
+}
