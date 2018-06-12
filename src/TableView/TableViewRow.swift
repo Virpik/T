b26082ecabb78656caa360ea7b630e69be44a8e3
vvirpik <ach@sybis.ru>
2018-06-12 20:09:02 +0700

    [DEV] Fix modifer access

--- src/TableView/TableViewRow.swift
+++ src/TableView/TableViewRow.swift
@@ -7,18 +7,24 @@
 
 import Foundation
 
-struct TableViewRow <Cell: UITableViewCell>: RowModelBlocks {
+public struct TableViewRow <Cell: UITableViewCell>: RowModelBlocks {
     
-    typealias RowType = Cell
+    public typealias RowType = Cell
     
-    var didSelect: ((Cell, IndexPath) -> Void)?
-    var build: ((Cell, IndexPath) -> Void)?
+    public var didSelect: ((Cell, IndexPath) -> Void)?
+    public var build: ((Cell, IndexPath) -> Void)?
     
-    func build(cell: Cell, indexPath: IndexPath) {
+    public var isNib: Bool = false
+    
+    public init() {
+        
+    }
+    
+    public func build(cell: Cell, indexPath: IndexPath) {
         self.build?(cell, indexPath)
     }
     
-    func didSelect(cell: Cell, indexPath: IndexPath) {
+    public func didSelect(cell: Cell, indexPath: IndexPath) {
         self.didSelect?(cell, indexPath)
     }
 }
