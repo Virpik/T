e4d34b08634fdff7f6ee5f0be39d41efe8fd3b62
vvirpik <ach@sybis.ru>
2018-06-14 15:20:17 +0700

    [DEV] Attempt to upgrade UIviewcontroller

---
+++ src/[UIViewController]/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  T
+//
+//  Created by Virpik on 14/06/2018.
+//
+
+import Foundation
+/**
+public protocol ViewControllerInterface {
+    
+    var blockViewDidLoad: Block? { get set }
+    
+    var blockViewWillAppear: BlockForItem<Bool>? { get set }
+    var blockViewDidAppear: BlockForItem<Bool>? { get set }
+    
+    var blockViewWillDisappear: BlockForItem<Bool>? { get set }
+    var blockViewDidDisappear: BlockForItem<Bool>? { get set }
+    
+    var blockViewDidLayoutSubview: Block? { get set }
+    var blockViewWillLayoutSubviews: Block? { get set }
+    
+    var isViewLoaded: Bool { get }
+    
+    func viewDidLoad()
+    
+    func viewWillAppear(_ animated: Bool)
+    func viewDidAppear(_ animated: Bool)
+    
+    func viewWillDisappear(_ animated: Bool)
+    func viewDidDisappear(_ animated: Bool)
+    
+    func viewDidLayoutSubviews()
+    func viewWillLayoutSubviews()
+}
+
+
+extension ViewControllerInterface {
+    public func viewDidLoad() {}
+    
+    public func viewWillAppear(_ animated: Bool) {}
+    public func viewDidAppear(_ animated: Bool) {}
+    
+    public func viewWillDisappear(_ animated: Bool) {}
+    public func viewDidDisappear(_ animated: Bool) {}
+    
+    public func viewDidLayoutSubviews() {}
+    public func viewWillLayoutSubviews() {}
+}
+*/
